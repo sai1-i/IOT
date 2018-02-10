ee7751742458be3e5cf154b561ed5a5452272b13
sai1-i <36275449+sai1-i@users.noreply.github.com>
2018-02-10 13:14:14 +0530

    Add files via upload

---
+++ Led using PIR.ino
@@ -0,0 +1,36 @@
+int pir=9;
+int state=LOW;
+int led=8;
+void setup()
+{
+  pinMode(pir, INPUT);
+  pinMode(8,OUTPUT);
+  Serial.begin(9600);
+  
+}
+void loop()
+{
+  int value=digitalRead(9);
+  if(value==HIGH)
+  {
+    digitalWrite(led, HIGH);
+    delay(100);
+    if(state==LOW)
+    {
+     Serial.println("motion detected");
+     state=HIGH;
+     }
+  }
+  else
+  {
+   digitalWrite(led, LOW);
+   delay(100);
+   if(state==HIGH)
+   {
+    Serial.println("motion not detected");
+    state=LOW;
+    }
+    
+  }
+}
+
